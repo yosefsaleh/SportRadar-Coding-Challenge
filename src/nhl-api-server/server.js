43c330b8e4ae766e9072a54eaba681c535ecdf50
Yosef Saleh <yosef426@hotmail.com>
2023-06-19 18:08:02 -0400

    started writing schedule api call in tandem with boxscore api call

--- src/nhl-api-server/server.js
+++ src/nhl-api-server/server.js
@@ -27,7 +27,7 @@ app.get('/api/game/:game_id/boxscore', async (req, res) => {
 app.get('/api/people/:person_id/stats/:season', async (req, res) => {
     try {
         const response = await axios.get(`https://statsapi.web.nhl.com/api/v1/people/${req.params.person_id}/stats?stats=statsSingleSeason&season=${req.params.season}`);
-        
+
         const data = response.data.stats[0].splits[0].stat;
 
         const result = {
@@ -37,7 +37,7 @@ app.get('/api/people/:person_id/stats/:season', async (req, res) => {
             Points: data.points,
             PenaltyMinutes: data.penaltyMinutes
         };
-        
+
         console.log(result);
         res.json(result);
     } catch (error) {
@@ -46,9 +46,40 @@ app.get('/api/people/:person_id/stats/:season', async (req, res) => {
     }
 });
 
+app.get('/api/schedule', async (req, res) => {
+    const season = req.query.season;  // Extract the 'season' query parameter from the request
+    let playerStats = [];
+    try {
+        const response = await axios.get(`https://statsapi.web.nhl.com/api/v1/schedule?season=${season}`);
+        const dates = response.data.dates;
+        for (let date of dates.slice(0, 5)) { // get only the first 5 games
+            const games = date.games;
+            for (let game of games) {
+                const boxscoreResponse = await axios.get(`https://statsapi.web.nhl.com/api/v1/game/${game.gamePk}/boxscore`);
+                const players = { ...boxscoreResponse.data.teams.home.players, ...boxscoreResponse.data.teams.away.players };
+                Object.values(players).forEach(player => {
+                    if (player.stats && player.stats.skaterStats) { // check if stats exist for the player
+                        const stats = player.stats.skaterStats;
+                        playerStats.push({
+                            personId: player.person.id,
+                            fullName: player.person.fullName,
+                            goals: stats.goals,
+                            assists: stats.assists,
+                        });
+                    }
+                });
+            }
+        }
+        console.log(playerStats);
+        res.json({ status: 'done' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'An error occurred while trying to fetch the schedule.' });
+    }
+});
 
 
 app.listen(port, () => {
     console.log(`App is running on http://localhost:${port}`);
-    startScheduleMonitoring();
+    //startScheduleMonitoring();
 });
