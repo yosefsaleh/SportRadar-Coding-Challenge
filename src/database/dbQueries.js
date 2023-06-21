5fd05a256cf92efb7064ddc90c23039ce3bd0f4a
Yosef Saleh <yosef426@hotmail.com>
2023-06-21 17:05:43 -0400

    added methods to store data in player and stats tables

--- src/database/dbQueries.js
+++ src/database/dbQueries.js
@@ -34,7 +34,48 @@ async function storeTeamData(team) {
     }
 }
 
+async function storePlayerData(playerData) {
+    try {
+        // Check if team_id is not "undefined"
+        if (playerData.team_id !== "undefined") {
+            const playerExists = await db.oneOrNone('SELECT id FROM players WHERE id = $1', [playerData.id]);
+
+            if (!playerExists) {
+                const queryText = 'INSERT INTO players(id, name, age, number, position, team_id) VALUES($1, $2, $3, $4, $5, $6)';
+                const values = [playerData.id, playerData.name, playerData.age, playerData.number, playerData.position, playerData.team_id];
+
+                await db.none(queryText, values);
+            }
+        } else {
+            console.log(`Player with ID: ${playerData.id} has an undefined team_id`);
+        }
+    } catch (error) {
+        console.error(`Error storing player data: ${error}`);
+    }
+}
 
 
-module.exports = { storeGameData, storeTeamData };
+
+async function storePlayerGameStats(player_id, game_id, stats, opponent_team_id) {
+    if (!opponent_team_id || opponent_team_id === "undefined") {
+        console.log("Player game stats not stored due to undefined field:", stats);
+        return;
+    }
+    
+    try {
+        const queryText = 'INSERT INTO player_game_stats(player_id, game_id, goals, assists, hits, points, penalty_minutes, opponent_team_id) VALUES($1, $2, $3, $4, $5, $6, $7, $8)';
+        const values = [player_id, game_id, stats.goals, stats.assists, stats.hits, stats.goals + stats.assists, stats.penaltyMinutes, opponent_team_id];
+
+        await db.none(queryText, values);
+    } catch (error) {
+        console.error(`Error storing player game stats: ${error}`);
+    }
+}
+
+module.exports = {
+    storeTeamData,
+    storeGameData,
+    storePlayerData,
+    storePlayerGameStats
+};
 
--- src/nhl-api-server/server.js
+++ src/nhl-api-server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const axios = require('axios');
 const app = express();
 const port = 8000;
-const { storeGameData, storeTeamData } = require('../database/dbQueries');
+const { storeGameData, storeTeamData, storePlayerData, storePlayerGameStats } = require('../database/dbQueries');
 const startScheduleMonitoring = require('./scheduleMonitor');
 
 app.get('/api/team/:team_id', async (req, res) => {
@@ -61,35 +61,46 @@ app.get('/api/schedule', async (req, res) => {
                 await storeGameData(game);
                 const boxscoreResponse = await axios.get(`https://statsapi.web.nhl.com/api/v1/game/${game.gamePk}/boxscore`);
                 const players = { ...boxscoreResponse.data.teams.home.players, ...boxscoreResponse.data.teams.away.players };
-                Object.values(players).forEach(player => {
+                Object.values(players).forEach(async (player) => {
                     if (player.stats && player.stats.skaterStats) { // check if stats exist for the player
                         const stats = player.stats.skaterStats;
+                        const opponent_team_id = player.person.currentTeam ? 
+                         (game.teams.home.team.id === player.person.currentTeam.id ? 
+                          game.teams.away.team.id : 
+                          game.teams.home.team.id) : "undefined";
+
+                        const playerData = {
+                            id: player.person.id,
+                            name: player.person.fullName,
+                            age: player.person.currentAge,
+                            number: player.jerseyNumber,
+                            position: player.position.code,
+                            team_id: player.person.currentTeam ? player.person.currentTeam.id : "undefined",
+                        };
+                        
+                        await storePlayerData(playerData, playerData.team_id, player.person.currentTeam ? player.person.currentTeam.name : "undefined");
+                        await storePlayerGameStats(player.person.id, game.gamePk, stats, opponent_team_id);
+                        
                         playerStats.push({
-                            personId: player.person.id,
-                            fullName: player.person.fullName,
-                            team_id: player.person.currentTeam ? player.person.currentTeam.id : null,
-                            team_name: player.person.currentTeam ? player.person.currentTeam.name : null,
-                            player_age: player.person.currentAge,
-                            player_number: player.jerseyNumber,
-                            player_position: player.position.code,
+                            ...playerData,
                             goals: stats.goals,
                             assists: stats.assists,
                             hits: stats.hits,
                             points: stats.goals + stats.assists,
                             penalty_minutes: stats.penaltyMinutes,
-                            opponent_team: player.person.currentTeam ? (game.teams.home.team.id === player.person.currentTeam.id ? game.teams.away.team.name : game.teams.home.team.name) : null,
+                            opponent_team_id: opponent_team_id
                         });
                     }
                 });
             }
         }
-        //console.log(playerStats);
         res.json(playerStats);
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: 'An error occurred while trying to fetch the schedule.' });
     }
 });
+
 
 
 app.listen(port, () => {
