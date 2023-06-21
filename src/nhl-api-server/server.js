547c98247e83a38e4710b9f65caddbb3847bac0c
Yosef Saleh <yosef426@hotmail.com>
2023-06-21 17:12:01 -0400

    cleaned up server.js file

--- src/nhl-api-server/server.js
+++ src/nhl-api-server/server.js
@@ -5,48 +5,6 @@ const port = 8000;
 const { storeGameData, storeTeamData, storePlayerData, storePlayerGameStats } = require('../database/dbQueries');
 const startScheduleMonitoring = require('./scheduleMonitor');
 
-app.get('/api/team/:team_id', async (req, res) => {
-    try {
-        const response = await axios.get(`https://statsapi.web.nhl.com/api/v1/teams/${req.params.team_id}`);
-        res.json(response.data.teams[0]);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'An error occurred while trying to fetch the team data.' });
-    }
-});
-
-app.get('/api/game/:game_id/boxscore', async (req, res) => {
-    try {
-        const response = await axios.get(`https://statsapi.web.nhl.com/api/v1/game/${req.params.game_id}/boxscore`);
-        res.json(response.data);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'An error occurred while trying to fetch the game boxscore.' });
-    }
-});
-
-app.get('/api/people/:person_id/stats/:season', async (req, res) => {
-    try {
-        const response = await axios.get(`https://statsapi.web.nhl.com/api/v1/people/${req.params.person_id}/stats?stats=statsSingleSeason&season=${req.params.season}`);
-
-        const data = response.data.stats[0].splits[0].stat;
-
-        const result = {
-            Assists: data.assists,
-            Goals: data.goals,
-            Hits: data.hits,
-            Points: data.points,
-            PenaltyMinutes: data.penaltyMinutes
-        };
-
-        console.log(result);
-        res.json(result);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'An error occurred while trying to fetch the person\'s stats.' });
-    }
-});
-
 app.get('/api/schedule', async (req, res) => {
     const season = req.query.season;  // Extract the 'season' query parameter from the request
     let playerStats = [];
